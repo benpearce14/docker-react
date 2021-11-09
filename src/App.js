27c78e0cb88a69ae77cfd10a6c6671d21843ebd2
Ben Pearce <b.j.pearce@durham.ac.uk>
2021-11-09 18:03:10 +0800

    Changed footer

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the feature branch
+          I was changed on the feature branch again!
         </a>
       </header>
     </div>
